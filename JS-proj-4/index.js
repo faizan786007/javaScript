0710ee04a5c247bf2fc3dca0187834283f2b57c2
faizan786007 <kingsmanfaizanali@gmail.com>
2024-08-20 20:06:14 +0530

    todos2

--- JS-proj-4/index.js
+++ JS-proj-4/index.js
@@ -1,4 +1,6 @@
-let items = ["milk", "lunch", "fruits"];
+// { name: "milk", date: "19-12-2023" },
+// 	{ name: "fruits", date: "1-1-2023" },
+let items = [];
 
 let data = document.getElementById("data");
 
@@ -7,7 +9,7 @@ function todo() {
 	items.map((item, index) => {
 		data.innerHTML += `
           <div >
-                <li >${item}</li>
+                <li >${item.name}<span class="item-date">${item.date}</span></li>
                 <button class="delete-button" onclick="deleteItem(${index})">Delete</button>
             </div>
         `;
@@ -17,12 +19,15 @@ todo();
 
 function addItem() {
 	let inputItem = document.getElementById("inputItem");
+	let inputDate = document.getElementById("inputDate");
 	let item = inputItem.value;
+	let date = inputDate.value;
 	// console.log(item);
 	// items.push(item);
-	items = [...items, item];
+	items = [...items, { name: item, date: date }];
 	todo();
 	inputItem.value = "";
+	inputDate.value = "";
 }
 function deleteItem(index) {
 	items.splice(index, 1);
